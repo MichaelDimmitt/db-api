3ccab51097d55a1bbc566440c25600e65ebce2cb
michael dimmitt <michaelgdimmitt@gmail.com>
2019-05-27 21:16:14 -0400

    enable cross origin for the server and change request to a post request.

--- express.js
+++ express.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const port = 6789
+
 let { postgresRoutes } = require('./routes/postgres.js');
 
 let app = express();
-app.listen(port);
+
+app.listen(port)
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+})
+
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
 console.log('app listening on port ' + port)
 
 app.get('/', function(req, res){
--- routes/postgres.js
+++ routes/postgres.js
@@ -1,12 +1,13 @@
 let app = require('express').Router();
 const { Client } = require('pg')
 const { config } = require('../config.js')
-app.get('/users', async function(req, res){
+app.post('/users', async function(req, res){
+  console.log({info: req.body})
   try{
   const client = new Client(config)
 
   await client.connect()
-  const body = await client.query(`SELECT * FROM newtravel`)
+  const body = await client.query(`SELECT * FROM users`)
   console.log(body.rows)
   await client.end()
   res.status(200).json(body.rows)
@@ -32,7 +33,7 @@ app.get('/insert', async function(req, res){
 //  const body2 = await client.query(`INSERT INTO users VALUES ('${params.firstName}', 'dimmitt', '82138','2312 baymeadows way, jacksonville, fl','9022006567','michaelgdimmitt@gmail.com');`)
   const body2 = await client.query(`INSERT INTO users VALUES ( '${params.firstName}', '${params.lastName}', '${params.npiNumber}', '${params.businessAddress}', '${params.telephoneNumber}', '${params.emailAddress}' )`);
   const body = await client.query('SELECT * FROM users')
-  console.log(body.rows[0])
+  console.log(body.rows)
   await client.end()
   res.status(200).json(body.rows)
   }
