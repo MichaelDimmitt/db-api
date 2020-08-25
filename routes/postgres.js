9c3708d32b38b0236d8807b2c380b019599fbd26
Michael Dimmitt <michaeldimmitt@Michaels-MacBook-Pro.local>
2020-08-25 14:22:46 -0400

    refactor, remove nested async await

--- routes/postgres.js
+++ routes/postgres.js
@@ -44,17 +44,18 @@ app.post('/insert', async function(req, res){
 
 app.get('/insertRepos', async function(req, res){
   try{
-  const client = new Client(config)
-  await client.connect()
-  const repos = await fetch(`https://api.github.com/users/michaeldimmitt/repos?per_page=100&page=2`)
-      .then(function(response) {
-        return response.json();
-      })
-      .then(async function(repos) {
-        try {
-        let reallyLargeString = 'INSERT INTO users VALUES'
-        const trimmedData = repos.map( repo => {
-
+    const client = new Client(config)
+    
+    await client.connect()
+    
+    const repos = await fetch(`https://api.github.com/users/michaeldimmitt/repos?per_page=100&page=2`)
+        .then(function(response) {
+          return response.json();
+        })
+    let msg = ''
+    let reallyLargeString = 'INSERT INTO users VALUES'
+    if(repos.length > 0){
+      const trimmedData = repos.map( repo => {
         const newRepo = {
             repo: repo.name,
             username: repo.owner.login,
@@ -63,24 +64,22 @@ app.get('/insertRepos', async function(req, res){
             languagecolor: "#89e051",
             description: repo.description === null ? '' : repo.description
           }
-        reallyLargeString = `${reallyLargeString} ( '${newRepo.repo}', '${newRepo.userName}', '${newRepo.starCount}', '${newRepo.majorityLanguage}', '${newRepo.languageColor}', '${newRepo.description}', )`;
+        
+        // reallyLargeString = `${reallyLargeString} ( '${newRepo.repo}', '${newRepo.userName}', '${newRepo.starCount}', '${newRepo.majorityLanguage}', '${newRepo.languageColor}', '${newRepo.description}', )`;
         return newRepo
-        })
-        const keyValues =Object.keys(trimmedData[0])
-        console.log({trimmedData, reallyLargeString, keyValues})
-
-        await client.executeBulkInsertion(trimmedData,keyValues,'repos');
-        res.status(200).json('done')
-        }
-        catch(err) {
-          console.log({err})
-          client.end()
-          res.status(501).json('hi')
-        }
       })
-      // psql -d project1 -c "SELECT *  FROM repos"
-      // psql -d project1 -c "DELETE from repos;"
+      const keyValues = Object.keys(trimmedData[0])
+      console.log({trimmedData, reallyLargeString, keyValues})
+
+      await client.executeBulkInsertion(trimmedData,keyValues,'repos');
+      msg = 'done'
+    }
+    else { msg = 'done, no insertion added' }
+    res.status(200).json(msg)
+    // psql -d project1 -c "SELECT *  FROM repos"
+    // psql -d project1 -c "DELETE from repos;"
   }
+   
   catch(err) {
     console.log({err})
     client.end()
